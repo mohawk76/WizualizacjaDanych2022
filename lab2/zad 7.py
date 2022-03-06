9a88e166c343dcc09965fe69156bb2705e109526
Michał Nowakowski <m98.nowakowski@gmail.com>
2022-03-06 15:54:35 +0100

    Dodanie treści zadania do lab 2, zad 7 oraz zmiana nazwy zmiennej

--- lab2/zad 7.py
+++ lab2/zad 7.py
@@ -1,2 +1,5 @@
-list = [1, 2, 3.14, 4, 5.22]
-print([i ** 2 for i in list])
+# Napisz skrypt, gdzie stworzysz listę składającą się z liczb typu int i float.
+# Następnie za pomocą użycia pętli for podnieś każdą liczbę do kwadratu.
+
+number_list = [1, 2, 3.14, 4, 5.22]
+print([i ** 2 for i in number_list])
