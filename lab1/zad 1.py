d4559d4fe26d833d1cc2310c9acc0bfec9f426b4
Michał Nowakowski <m98.nowakowski@gmail.com>
2022-03-06 15:53:00 +0100

    Dodanie treści do lab 1, zad 1 oraz uzupełnienie o brakujące zmienne

--- lab1/zad 1.py
+++ lab1/zad 1.py
@@ -1,10 +1,25 @@
+# Napisz pierwszy skrypt,w którym zadeklarujesz po dwie zmienne każdego typu, a następnie wyświetl te zmienne
 string_variable = "string"
-int_variable = 5
-float_variable = 1.5
-complex_variable = 1 + 1j
+string_variable2 = "string2"
 
-print("String value: %s" % string_variable)
-print("Integer value: %d" % int_variable)
-print("Float value: %f" % float_variable)
+int_variable = 5
+int_variable2 = 6
+
+float_variable = 1.5
+float_variable2 = 3.14
+
+complex_variable = 1 + 1j
+complex_variable2 = 3 + 1j
+
+print("Pierwszy ciąg znaków: %s" % string_variable)
+print("Drugi ciąg znaków: %s" % string_variable2)
+
+print("Pierwsza liczba całkowita: %d" % int_variable)
+print("Druga liczba całkowita: %d" % int_variable2)
+
+print("Pierwsza liczba zmiennoprzecinkowa: %f" % float_variable)
+print("Druga liczba zmiennoprzecinkowa: %f" % float_variable2)
+
 # Rzutowanie complex na string
-print("Complex value: %s" % complex_variable)
+print("Pierwsza liczba urojona: %s" % complex_variable)
+print("Druga liczba urojona: %s" % complex_variable2)
