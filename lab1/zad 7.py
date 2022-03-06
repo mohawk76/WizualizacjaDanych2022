951c9e7fa7b94eaa33e8891ffe9a58066a9040b3
Michał Nowakowski <m98.nowakowski@gmail.com>
2022-03-06 15:54:00 +0100

    Dodanie treści do lab 1, zad 7 oraz zmiana komunikatów

--- lab1/zad 7.py
+++ lab1/zad 7.py
@@ -1,4 +1,8 @@
+# Do poszczególnych elementów łańcucha możemy się odwołać przez podanie indeksu. Np. pierwszy znak zapisany w zmiennej
+# imie uzyskamy poprzez imie[0]. Zapisz dowolną zmienną łańcuchową i wyświetl jej drugą i ostatnią literę, wykorzystując
+# indeksy
+
 name = "Michał"
 
-print(name[1])  # druga litera
-print(name[-1])  # ostatnia litera
+print("Druga litera: %s" % name[1])
+print("Ostatnia litera: %s" % name[-1])
